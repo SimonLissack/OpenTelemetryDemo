5312bc910eb3a90d1067db3f4404e7bef2e0611d
Simon Lissack <lissack.simon@gmail.com>
2024-08-17 20:19:23 +0100

    Create new service to write out state as logs

---
+++ OpenTelemetryDemo.Cli/Services/LogRenderService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpenTelemetryDemo.Domain.Abstractions;
+using OpenTelemetryDemo.Domain.TrafficLights.Models;
+
+namespace OpenTelemetryDemo.Cli.Services;
+
+public class LogRenderService(ILogger<LogRenderService> logger, IRepository<TrafficLight> repository) : BackgroundService
+{
+    readonly TimeSpan _sleepTimer = TimeSpan.FromSeconds(1);
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Render(stoppingToken);
+            await Task.Delay(_sleepTimer, stoppingToken);
+        }
+    }
+
+    async Task Render(CancellationToken cancellationToken)
+    {
+        await foreach (var trafficLight in repository.GetAllAsync(cancellationToken))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            logger.LogDebug("{TrafficLight} is {State} since {LastTransition} and has {QueuedCars} vehicles queued",
+                trafficLight.Name,
+                trafficLight.LightState,
+                trafficLight.LastTransition,
+                trafficLight.QueuedTraffic
+            );
+        }
+    }
+}
